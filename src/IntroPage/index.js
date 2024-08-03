4b0c1ae3615e3b220600a67c45e73e27f3b2b3ea
ellie <ellie@ellies-MacBook-Pro.local>
2024-08-03 14:43:42 +0900

    Description below

--- src/IntroPage/index.js
+++ src/IntroPage/index.js
@@ -1,31 +1,70 @@
 import styled from "styled-components";
 import React, { useState } from "react";
 import { slide as Menu } from "react-burger-menu";
+import IndexPage from "../IndexPage";
+import RedHeart from "./redheart.png";
 
 const Container = styled.div`
     height: 100vh;
     background-color: #aeb9bf;
     display: flex;
-    flex-direction: row;
-`;
-const LeftContainer = styled.div`
-    width: 50%;
-    background-size: cover;
-    height: 100%;
-`;
-const RightContainer = styled.div`
-    width: 50%;
-    display: flex;
-    flex-direction: column; 
-    justify-content: center;
-    align-items: center;
+    flex-direction: column;
 `;
 
+const Title = styled.div`
+font-size: 100px;
+margin-left: 30%;
+margin-top: 40px;
+
+`;
+
+const TopContainer = styled.div`
+display:flex;
+flex-direction: row;
+margin-left: 10%;
+margin-top: 50px;
+`;
+
+const Purpose = styled.div`
+
+font-size: 40px;
+
+`;
+
+const ImageContainer = styled.div`
+margin-left: 20px;
+display:flex;
+flex-direction: column;
+
+align-items: center;
+`;
+
+const Image = styled.img`
+width: 200px;
+`;
+
+const Link = styled.a`
+
+`;
 function IntroPage(){
     return(
         <Container>
-            <LeftContainer></LeftContainer>
-            <RightContainer></RightContainer>
+            <IndexPage></IndexPage>
+            <Title>WPR Jeju</Title>
+            <TopContainer>
+                <Purpose>
+                    sdddffdfdfdfdf<br></br>df
+                sdddffdfdfdfdf<br></br>
+                dfsdddffdfdfdfdf<br></br>dfsdddffdfdfdfdf
+                <br></br>dfsdddffdfdfdfdf<br></br>df
+                </Purpose>
+                <ImageContainer>
+                    <Image src={RedHeart}/>
+                    <Link href="https://www.google.co.kr/?hl=ko">click</Link>
+                </ImageContainer>
+           
+           </TopContainer>
+           
         </Container>
     );
 };
